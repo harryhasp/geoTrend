b55b2e3cd21c6b25298486dbaa19905ec9884b4b
harry <z.hasparis@gmail.com>
2018-01-21 18:03:46 -0800

    Initial

--- src/mySystem/projectMain.java
+++ src/mySystem/projectMain.java
@@ -1,4 +1,13 @@
 package mySystem;
 
 public class projectMain {
+
+    public static void main(String[] args) {
+        String sampleDataFile = "sampleData.txt" ;
+        String newDdataFile = "" ;
+
+        indexShapingClass indexShape = new indexShapingClass() ;
+        indexShape.indexShaping(sampleDataFile) ;
+    }
+
 }
